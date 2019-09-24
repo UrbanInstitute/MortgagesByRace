4f23d7f845ba4d36fd8403082384425cceab0f9d
Feng <afeng@urban.org>
2019-09-24 16:32:49 -0400

    Add scripts to help with data processing

---
+++ data/makeMbtiles.sh
@@ -0,0 +1,30 @@
+## Shell script for creating mbtiles for HMDA map
+
+#!/bin/bash
+
+# change this as needed
+year=2018
+
+# make African-American purchase mbtiles
+tippecanoe -o us-purchase$year-aa.mbtiles -z11 -Z3 -r1.5 -g3 -x raceethnic us-purchase$year-aa.geojson
+
+# make Asian-American purchase mbtiles
+tippecanoe -o us-purchase$year-asian.mbtiles -z11 -Z3 -r1.5 -g3 -x raceethnic us-purchase$year-asian.geojson
+
+# make Hispanic purchase mbtiles
+tippecanoe -o us-purchase$year-his.mbtiles -z11 -Z3 -r1.5 -g3 -x raceethnic us-purchase$year-his.geojson
+
+# make non-Hispanic white purchase mbtiles
+tippecanoe -o us-purchase$year-nhw.mbtiles -z11 -Z3 -r1.5 -g3 -x raceethnic us-purchase$year-nhw.geojson
+
+# make African-American refinance mbtiles
+tippecanoe -o us-refi$year-aa.mbtiles -z11 -Z3 -r1.5 -g3 -x raceethnic us-refi$year-aa.geojson
+
+# make Asian-American refinance mbtiles
+tippecanoe -o us-refi$year-asian.mbtiles -z11 -Z3 -r1.5 -g3 -x raceethnic us-refi$year-asian.geojson
+
+# make Hispanic refinance mbtiles
+tippecanoe -o us-refi$year-his.mbtiles -z11 -Z3 -r1.5 -g3 -x raceethnic us-refi$year-his.geojson
+
+# make non-Hispanic white refinance mbtiles
+tippecanoe -o us-refi$year-nhw.mbtiles -z11 -Z3 -r1.5 -g3 -x raceethnic us-refi$year-nhw.geojson
---
+++ data/prepDataForQGIS.R
@@ -0,0 +1,28 @@
+# This script prepares the data for use in Mapbox.
+# It separates the source data provided from HFPC into
+# 8 csv files by race and mortgage type (purchase or refinance).
+
+library(tidyverse)
+
+year <- 2018  # change year as needed to reflect most recent data
+
+purchase_raw <- read_csv(str_glue("source/us-purchase{year}.csv"))
+refi_raw <- read_csv(str_glue("source/us-refi{year}.csv"))
+
+purchase_dat <- purchase_raw %>%
+  select(-tract, -total, -loantype) %>%
+  group_split(raceethnicity)
+
+write_csv(purchase_dat[[1]], str_glue("us-purchase{year}-aa.csv"))
+write_csv(purchase_dat[[2]], str_glue("us-purchase{year}-asian.csv"))
+write_csv(purchase_dat[[3]], str_glue("us-purchase{year}-his.csv"))
+write_csv(purchase_dat[[4]], str_glue("us-purchase{year}-nhw.csv"))
+
+refi_dat <- refi_raw %>%
+  select(-tract, -total, -loantype) %>%
+  group_split(raceethnicity)
+
+write_csv(refi_dat[[1]], str_glue("us-refi{year}-aa.csv"))
+write_csv(refi_dat[[2]], str_glue("us-refi{year}-asian.csv"))
+write_csv(refi_dat[[3]], str_glue("us-refi{year}-his.csv"))
+write_csv(refi_dat[[4]], str_glue("us-refi{year}-nhw.csv"))
